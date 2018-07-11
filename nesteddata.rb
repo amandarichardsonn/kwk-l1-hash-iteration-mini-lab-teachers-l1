9fb716ec3bf965d56328ac8d6fe20f643b2c5bcd
Amanda Richardson <amanda162000@gmail.com>
2018-07-11 20:43:07 +0000

    automatically backed up by learn

---
+++ nesteddata.rb
@@ -0,0 +1,7 @@
+KlossyPosse = [
+ ["Caroline", "Vivian", "Jessica", "Madhu"],
+ ["Azya ", "Natalie", "ChaCha", "Claudia"],
+ ["Marisa", "Citlally", "Olivia", "Clare"],
+ ["Sara", "Naquaiviera", "Zoe", "Karlie Kloss"],
+ ["Vlad", "Tierraney", "Janice", "Amanda"]
+]
